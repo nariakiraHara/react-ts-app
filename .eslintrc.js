17322deaa414b141f0cdbb73e998db306a8526e5
nhara <nhara@yahoo-corp.jp>
2021-03-16 23:51:51 +0900

    eslint追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['standard', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
-    },
-    "rules": {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    'simple-import-sort',
+    '@typescript-eslint',
+    'jsdoc'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
     }
+  },
+  rules: {
+  }
 }
