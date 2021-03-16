79f8f2c009c95e369875dc055de8f7b6fd87cb73
nhara <nhara@yahoo-corp.jp>
2021-03-16 23:15:30 +0900

    テストコード追加

--- src/tests/components/FormInput/FormInput.test.tsx
+++ src/tests/components/FormInput/FormInput.test.tsx
@@ -2,8 +2,16 @@ import renderer from 'react-test-renderer'
 import FormInput from 'app/components/FormInput'
 
 describe('FormInput', () => {
-  test('正常系', () => {
-    const tree = renderer.create(<FormInput text={'テストテキスト'} />).toJSON()
+  test('正常系 type=text', () => {
+    const tree = renderer.create(<FormInput type={'text'} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  test('正常系 type=email', () => {
+    const tree = renderer.create(<FormInput type={'email'} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  test('正常系 type=password', () => {
+    const tree = renderer.create(<FormInput type={'password'} />).toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
